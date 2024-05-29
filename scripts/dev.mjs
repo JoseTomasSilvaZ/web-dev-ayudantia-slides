e71c6fe8ce29d0a5efef340d86e378498797dce4
josetomassilvaz <josetomassilvaz@gmail.com>
2024-05-29 13:25:02 -0400

    feat: :sparkles: update index

--- scripts/dev.mjs
+++ scripts/dev.mjs
@@ -1,16 +1,14 @@
-import { $, cd, argv, echo } from 'zx'
+import { $, cd, argv, echo } from "zx";
 
-$.env.FORCE_COLOR = '1'
+$.env.FORCE_COLOR = "1";
 
-const [slide] = argv._
-
-
+const [slide] = argv._;
 
 if (slide) {
-  echo `Opening presentation: ${slide}`
-  await cd(`presentations/${slide}`)
+  echo`Opening presentation: ${slide}`;
+  await cd(`presentations/${slide}`);
 } else {
-  echo `Opening indexation of slides`
+  echo`Opening indexation of slides`;
 }
 
-await $`slidev index.md --log info --open`
+await $`serve .`;
